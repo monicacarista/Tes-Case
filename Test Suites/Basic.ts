6ad809ce0c3c6a6d04c4070621051931b0a6e0a2
monicacarista <monica.carista@si.ukdw.ac.id>
2020-03-17 17:47:39 +0700

    basic tes

---
+++ Test Suites/Basic.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Basic Test suite for CURA</description>
+   <name>Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f60a798a-1f0d-4155-840a-2e7c150a2bb2</testSuiteGuid>
+   <testCaseLink>
+      <guid>8a66de4e-aa08-4386-801f-406fefbf1317</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Basic</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
